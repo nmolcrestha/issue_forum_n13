89493201fe37abfffbacc6fce55f77a0d2e1dd1e
Anmol Shrestha <nmol.crestha@gmail.com>
2023-10-26 11:17:02 +0545

    Added clerk auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="h1-bold">The project is set.</h1>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
