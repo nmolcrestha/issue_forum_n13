7200917d81225fffd72e25e7d3ec499652aac2fe
Anmol Shrestha <nmol.crestha@gmail.com>
2023-11-02 07:03:39 +0545

    toggle dark/light mode set in NAV

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -8,8 +8,6 @@ import {
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import Image from "next/image";
@@ -42,7 +40,18 @@ const Theme = () => {
         </MenubarTrigger>
         <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
           {themes.map((item) => (
-            <MenubarItem key={item.value}>
+            <MenubarItem
+              key={item.value}
+              className="flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
+              onClick={() => {
+                setMode(item.value);
+                if (item.value !== "system") {
+                  localStorage.theme = item.value;
+                } else {
+                  localStorage.removeItem("theme");
+                }
+              }}
+            >
               <Image
                 src={item.icon}
                 alt={item.value}
@@ -50,6 +59,15 @@ const Theme = () => {
                 height={16}
                 className={`${mode === item.value && "active-theme"}`}
               />
+              <p
+                className={`body-semibold text-light-500 ${
+                  mode === item.value
+                    ? "text-primary-500"
+                    : "text-dark100_light900"
+                }`}
+              >
+                {item.label}
+              </p>
             </MenubarItem>
           ))}
         </MenubarContent>
