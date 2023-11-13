abeb61ff69390dd316646153cb00bd4a08689071
Anmol Shrestha <nmol.crestha@gmail.com>
2023-11-13 07:30:57 +0545

    No question component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,39 @@
 import Filter from "@/components/shared/Filter";
 import HomeFilter from "@/components/shared/HomeFilter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filter";
 import Link from "next/link";
+
+const questions = [
+  // {
+  //   _id: 1,
+  //   title: "Cascading Deletes in SQLAlchemy?",
+  //   tags: [
+  //     { _idL: 1, name: "python" },
+  //     { _id: 2, name: "sql" },
+  //   ],
+  //   author: "John Doe",
+  //   upvotes: 10,
+  //   views: 100,
+  //   answer: 2,
+  //   createdAt: "2022-02-01 T12:00:00:000Z",
+  // },
+  // {
+  //   _id: 1,
+  //   title: "How to center a div?",
+  //   tags: [
+  //     { _idL: 1, name: "css" },
+  //     { _id: 2, name: "html" },
+  //   ],
+  //   author: "Steve Lui",
+  //   upvotes: 5,
+  //   views: 170,
+  //   answer: 6,
+  //   createdAt: "2022-02-01 T12:00:00:000Z",
+  // },
+];
 
 export default function Home() {
   return (
@@ -31,6 +61,22 @@ export default function Home() {
           containerClasses="hidden max-md:flex"
         />
         <HomeFilter />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => "Question")
+        ) : (
+          <NoResult
+            title="There's no questions to show"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus fugit
+          autem, obcaecati ex magnam quibusdam. At nemo error aliquam vero,
+          veritatis, porro voluptate architecto provident minus, consectetur
+          pariatur repellendus non."
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
       </div>
     </>
   );
