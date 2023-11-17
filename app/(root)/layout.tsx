06f14105c2462f5161faf3d6e76698d2de57a9aa
Anmol Shrestha <nmol.crestha@gmail.com>
2023-11-17 07:00:31 +0545

    Added tinyeditoe

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className="flex">
         <LeftSideBar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
-          <div className="max-w-5x1 mx-auto w-full">{children}</div>
+          <div className="mx-auto w-full max-w-xl">{children}</div>
         </section>
         <RightSideBar />
       </div>
