d02cca7b76ced1aea7ecd4bb07daf5e0362aa030
Anmol Shrestha <nmol.crestha@gmail.com>
2023-12-08 21:59:24 +0545

    dialog fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,44 +6,30 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const getTimeStamp = (createdAt: Date): string => {
-  const timeDifference = Date.now() - createdAt.getTime();
+  const now: Date = new Date();
+  const timeDifference: number = now.getTime() - createdAt.getTime();
 
-  // Define time units in milliseconds
-  const minute = 60 * 1000;
-  const hour = 60 * minute;
-  const day = 24 * hour;
-  const month = 30 * day;
-  const year = 365 * day;
+  const seconds: number = Math.floor(timeDifference / 1000);
+  const minutes: number = Math.floor(seconds / 60);
+  const hours: number = Math.floor(minutes / 60);
+  const days: number = Math.floor(hours / 24);
 
-  // Determine the appropriate time unit and value
-  if (timeDifference < minute) {
-    const seconds = Math.floor(timeDifference / 1000);
-    return `${seconds} second${seconds === 1 ? "" : "s"} ago`;
-  } else if (timeDifference < hour) {
-    const minutes = Math.floor(timeDifference / minute);
-    return `${minutes} minute${minutes === 1 ? "" : "s"} ago`;
-  } else if (timeDifference < day) {
-    const hours = Math.floor(timeDifference / hour);
-    return `${hours} hour${hours === 1 ? "" : "s"} ago`;
-  } else if (timeDifference < month) {
-    const days = Math.floor(timeDifference / day);
+  if (days > 0) {
     return `${days} day${days === 1 ? "" : "s"} ago`;
-  } else if (timeDifference < year) {
-    const months = Math.floor(timeDifference / month);
-    return `${months} month${months === 1 ? "" : "s"} ago`;
+  } else if (hours > 0) {
+    return `${hours} hour${hours === 1 ? "" : "s"} ago`;
+  } else if (minutes > 0) {
+    return `${minutes} minute${minutes === 1 ? "" : "s"} ago`;
   } else {
-    const years = Math.floor(timeDifference / year);
-    return `${years} year${years === 1 ? "" : "s"} ago`;
+    return `${seconds} second${seconds === 1 ? "" : "s"} ago`;
   }
 };
 
 export const formatBigNumber = (inputNumber: number): string => {
   if (inputNumber >= 1000000) {
-    const millionValue = (inputNumber / 1000000).toFixed(2);
-    return `${millionValue}M`;
+    return (inputNumber / 1000000).toFixed(1) + "M";
   } else if (inputNumber >= 1000) {
-    const thousandValue = (inputNumber / 1000).toFixed(2);
-    return `${thousandValue}K`;
+    return (inputNumber / 1000).toFixed(1) + "k";
   } else {
     return inputNumber.toString();
   }
